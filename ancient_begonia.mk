1598e8b346f8190dd7f17afc135a4f9292e8082b
stellabyte <psambitk1@gmail.com>
2021-11-19 06:09:20 +0000

    begonia: AncientOS

--- ancient_begonia.mk
+++ ancient_begonia.mk
@@ -28,11 +28,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, device/redmi/begonia/device.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := potato_begonia
+PRODUCT_NAME := ancient_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
@@ -45,3 +45,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="begonia"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+ANCIENT_OFFICIAL := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
